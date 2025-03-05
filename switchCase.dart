615b2f8b9e4c3f4ac9baf37f15d4ede3faab806d
Miguele73 <cabusam.eu@gmail.com>
2025-03-05 11:04:37 +0100

    switchCases Aufgabe Weekday

---
+++ switchCase.dart
@@ -0,0 +1,18 @@
+enum Weekday { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
+
+void main() {
+  Weekday? Wochentage = Weekday.monday;
+  switch (Wochentage) {
+    case Weekday.monday:
+    case Weekday.tuesday:
+      print('Montage und Dienstage sind anstrengend');
+    case Weekday.wednesday:
+    case Weekday.thursday:
+      print('Mittwoch und Donnerstag sind ok');
+    case Weekday.friday:
+      print('Freitag ist super');
+    case Weekday.saturday:
+    case Weekday.sunday:
+      print('Wochenende ist genial');
+  }
+}
