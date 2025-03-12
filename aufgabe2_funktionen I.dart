767f8b71f45ce180f7a0d42e15ee92e7d3a44b75
Miguele73 <cabusam.eu@gmail.com>
2025-03-12 11:37:03 +0100

    Aufgabe 2_ Funktionen I

---
+++ aufgabe2_funktionen I.dart
@@ -0,0 +1,11 @@
+void triplePrint(String text, int ammount) {
+  for (int i = 0; i < ammount; i++) {
+    print(text);
+  }
+}
+
+void main() {
+  triplePrint('Ich denke......', 3);
+  triplePrint('Ich denke immer noch......', 3);
+  triplePrint('Ich gebe es auf ;)', 3);
+}
