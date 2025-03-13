527868c184c70dbf5f29588aaa13a8eddbc3c720
Miguele73 <cabusam.eu@gmail.com>
2025-03-13 11:17:37 +0100

    funktionenII_aufgabe2

---
+++ funktionenII_aufgabe2.dart
@@ -0,0 +1,9 @@
+int sumOfLetters(String Text) {
+  return Text.length;
+}
+
+void main() {
+  print(sumOfLetters('Long Island Iced Tea'));
+  print(sumOfLetters('Sex on the Beach'));
+  print(sumOfLetters('Tequila Sunrise'));
+}
